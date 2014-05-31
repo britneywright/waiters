58be18b74e346f1e75a72d3481a2224315c493db
Chris Hoffman <choffman@optoro.com>
2014-05-31 15:09:31 -0400

    Cleaned up the modal_forms.js.
    Made the function naming idiomatic js. Also fixed a typo.

--- app/assets/javascripts/modal_forms.js
+++ app/assets/javascripts/modal_forms.js
@@ -1,13 +1,13 @@
 $(function () {
-  function modal_success (node) {
+  function modalSuccess (node) {
     node.modal('hide');
     node.find('form input[type="text"]').val('');
 
-    clear_previous_form_errors(node);
+    clearPreviousFormErrors(node);
   }
 
-  function render_form_errors (node, errors) {
-    clear_previous_form_errors(node);
+  function renderFormErrors (node, errors) {
+    clearPreviousFormErrors(node);
 
     for (var field in errors) {
       var control = $('input[name="' + field + '"]', node).parent();
@@ -18,26 +18,28 @@ $(function () {
     }
   }
 
-  function clear_previous_form_errors (node) {
+  function clearPreviousFormErrors (node) {
     $('.form-group.has-error', node).each(function () {
       $('.help-block', node).html('');
       node.removeClass('has-error');
     });
   }
 
-  $(document).bind('ajaxError', 'form', function modalAjaxError (event, jqxhr, settings, exception) {
+  function modalAjaxError (event, jqxhr, settings, exception) {
     // note: jqxhr.responseJSON undefined, parsing responseText instead
     var errors = $.parseJSON(jqxhr.responseText).errors;
 
     var model = Object.keys(errors)[0];
     var selector = "#new_" + model;
 
-    render_form_errors($(selector), errors[model]);
-  });
-
-  function modalAjaxSuccess (event, jqxhr, params, exception) {
-    clear_previous_form_errors($(event.data));
+    renderFormErrors($(selector), errors[model]);
   }
 
-  $(document).bind('ajaxSucess', 'form', modalAjaxSuccess);
+  function modalAjaxSuccess (event, jqxhr, params, exception) {
+    modalSuccess($(event.data));
+  }
+
+
+  $(document).bind('ajaxError', 'form', modalAjaxError);
+  $(document).bind('ajaxSuccess', 'form', modalAjaxSuccess);
 });
