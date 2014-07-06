3708f6cf7588a1261671502f6e17a0cae1180ae6
Britney Wright <blw06g@gmail.com>
2014-07-06 11:39:54 -0400

    formatting inquiry form functionality

--- app/assets/javascripts/inquiries.js
+++ app/assets/javascripts/inquiries.js
@@ -7,4 +7,16 @@ $( "input" ).on( "click", function() {
   $('[data-show-for='+ $( "input:checked" ).val() +']').toggle();
 });
 
+if ( $("#inquiry_role_hire").is(':checked')){
+  $( "#log" ).html( $( "input:checked" ).val() + " is checked!" );
+  $('[data-show-for=both]').show();
+  $('[data-show-for='+ $( "input:checked" ).val() +']').show();
+}
+
+if ( $("#inquiry_role_work").is(':checked')){
+  $( "#log" ).html( $( "input:checked" ).val() + " is checked!" );
+  $('[data-show-for=both]').show();
+  $('[data-show-for='+ $( "input:checked" ).val() +']').show();
+}
+
 });
